9683dec0142619ec21f902069f4bce99bfe5f0aa
Vaibhav Patel <vaibhav.p@mantralabsglobal.com>
2021-01-07 19:21:57 +0530

    Solved #22

--- #0022 Generate Parentheses/generate_parentheses.py
+++ #0022 Generate Parentheses/generate_parentheses.py
@@ -1,31 +1,15 @@
 class Solution:
     def generateParenthesis(self, n: int):
-        listOfParentheses = []
-        
-        parentheses = "".join(["("]*n+[")"]*n)
-        listOfParentheses.append(parentheses)
-
-        while "))" in parentheses:
-            index = parentheses.index("))")
-            parentheses = parentheses[:index-1]+ parentheses[index] + parentheses[index-1:index] + parentheses[index+1:]
-            if parentheses not in listOfParentheses:
-                listOfParentheses.append(parentheses)
-
-        parentheses = "".join(["("]*n+[")"]*n)
-        while "((" in parentheses: 
-            index = parentheses.rindex("((")
-            parentheses = parentheses[:index+1] + parentheses[index+2:index+3] + parentheses[index+1]  + parentheses[index+3:]
-            if parentheses not in listOfParentheses:
-                listOfParentheses.append(parentheses)
-
-        return listOfParentheses
+        generatedParenthesis = {1:["()"]}
+        for index in range(2,n+1):
+            generatedParenthesis[index] = []
+            for paranthesis in generatedParenthesis[index-1]:
+                generatedParenthesis[index].append("()"+paranthesis)
+                generatedParenthesis[index].append("("+paranthesis+")")
+            for pastParanthesisindex in range(1,index-1):
+                for pastParanthesis in generatedParenthesis[pastParanthesisindex]:
+                    for nextParanthesis in generatedParenthesis[index-1-pastParanthesisindex]:
+                        generatedParenthesis[index].append("("+pastParanthesis+")"+nextParanthesis)
+        return generatedParenthesis[n]
 
 print ("Solution\""+str(Solution().generateParenthesis(3))+"\"")
-
-
-Input
-4
-Output
-["(((())))","((()()))","((())())","(()()())","(()())()","(())()()","()()()()","(()(()))","()(()())","()()(())"]
-Expected
-["(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"]
