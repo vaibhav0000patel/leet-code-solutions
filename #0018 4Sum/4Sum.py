c4d3e3cc494572d8aa9c154f6f4e283af334b512
Vaibhav Patel <vaibhav.p@mantralabsglobal.com>
2021-01-07 19:21:31 +0530

    Solving #18

--- #0018 4Sum/4Sum.py
+++ #0018 4Sum/4Sum.py
@@ -1,17 +1,23 @@
 class Solution:
     def fourSum(self, nums, target):
-        if not nums or len(nums)<4:
-            return []
-        fourSumList = []
-        
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                for k in range(j+1,len(nums)):
-                    for l in range(k+1,len(nums)):
-                        sumList = [nums[i],nums[j],nums[k],nums[l]]
-                        sumList.sort()
-                        if sum(sumList)==target and sumList not in fourSumList:
-                            fourSumList.append(sumList)
-        return fourSumList
+        list_of_triplets = []
+        nums.sort()
+        for index,value in enumerate(nums):
+            if index>0 and value==nums[index-1]:
+                continue
+            left_pointer = index+1
+            right_pointer = len(nums)-1
+            while left_pointer < right_pointer:
+                three_sum = nums[left_pointer]+nums[index]+nums[right_pointer]
+                if three_sum<target:
+                    left_pointer+=1
+                elif three_sum>target:
+                    right_pointer-=1
+                else:
+                    list_of_triplets.append([nums[left_pointer],nums[index],nums[right_pointer]])
+                    left_pointer+=1
+                    while nums[left_pointer]==nums[left_pointer-1] and left_pointer<right_pointer:
+                        left_pointer+=1
+        return list_of_triplets
 
 print ("Solution\""+str(Solution().fourSum([1,0,-1,0,-2,2],0))+"\"")
